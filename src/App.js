30627876252dc2bd84fc6a178ec73716ede6d40c
ManzoorAhmad1 <manzoor8132@gmail.com>
2023-05-20 10:54:51 -0700

    ExpenseDate.js file added

--- src/App.js
+++ src/App.js
@@ -1,10 +1,19 @@
 import './App.css';
 import ExpensiveItem from "./Components/ExpensiveItems"
 function App() {
+  const expense=[
+    {id:'e',title:"Car Insurance",Date:new Date(2023,3,22),price:"234.23"},
+    {id:'e',title:"Tolit Paper",Date:new Date(2023,4,2),price:"345.23"},
+    {id:'e',title:"Study Table (wooden)",Date:new Date(2023,5,12),price:"123.23"},
+    {id:'e',title:"Books Collection",Date:new Date(2023,6,3),price:"155.23"}
+  ]
   return (
     <>
     <h1>Let get start</h1>
-    <ExpensiveItem></ExpensiveItem>
+    <ExpensiveItem title={expense[0].title} date={expense[0].Date} price={expense[0].price}/>
+    <ExpensiveItem title={expense[1].title} date={expense[1].Date} price={expense[1].price}/>
+    <ExpensiveItem title={expense[2].title} date={expense[2].Date} price={expense[2].price}/>
+    <ExpensiveItem title={expense[3].title} date={expense[3].Date} price={expense[3].price}/> 
     </>
   );
 }
