bde11b2838cabb061b6c29a5f7e59b972afc6b69
ManzoorAhmad1 <manzoor8132@gmail.com>
2023-06-02 02:46:18 -0700

    create ChartBar.css and ChartBar.js

--- src/Components/Expense/ExpenseFilter.js
+++ src/Components/Expense/ExpenseFilter.js
@@ -1,17 +1,19 @@
-import React from 'react';
-import "./ExpenseFilter.css"
+import React from "react";
+import "./ExpenseFilter.css";
 
 const ExpensesFilter = (props) => {
-
+  const dropDownChangeHandler=(event)=>{
+    props.onFilterData(event.target.value)
+  }
   return (
-    <div className='expenses-filter'>
-      <div className='expenses-filter__control'>
+    <div className="expenses-filter">
+      <div className="expenses-filter__control">
         <label> Filter by year</label>
-        <select>
-          <option value='2023' >2023</option>
-          <option value='2022'  >2022</option>
-          <option value='2021'>2021</option>
-          <option value='2020'>2020</option>
+        <select value={props.selected} onChange={dropDownChangeHandler}>
+          <option value="2023">2023</option>
+          <option value="2022">2022</option>
+          <option value="2021">2021</option>
+          <option value="2020">2020</option>
         </select>
       </div>
     </div>
